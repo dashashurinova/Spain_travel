14bfecd4d14ae9e057a201455210f367f9181978
dashashurinova <dasha.shurinova@yandex.ru>
2023-07-24 11:47:00 +0700

    popUpWindow

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-// document.getElementsByClassName('entrance').addEventListener ("submit",checkForm);
-// let entrance__button=document.querySelector('.entrance__button');
-// console.log(entrance__button);
-
-
-function checkForm(el) {
-    let login = document.querySelector('.login');
-    console.log(login);
-    let pass = document.querySelector('.password');
- let fail = "";
- if (login=="" || pass=="")
-    fail = "Enter login and password"; 
+function checkForm() {
+    let copy = document.querySelectorAll('.entrance__log');
+    let login =(copy[0].value);
+    let pass =(copy[1].value);
+    let fail = "";
+    if (login=="" || pass=="")
+        fail = "Enter login and password"; 
     else if (login.length <= 1)
-    fail = "Enter correct login";
-if (fail != "")
-document.getElementById("error").innerHTML = fail;
-console.log(fail);
+        fail = "Enter correct login";
+    if (fail != "")
+        document.getElementById("error").innerHTML = fail;
+    return fail;
 }
-
-checkForm();
